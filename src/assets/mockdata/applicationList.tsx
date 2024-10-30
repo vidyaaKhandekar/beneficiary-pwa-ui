abea4114f90cd0fee714ee8a021fe8acb738218b
vidya khandekar <vidya.khandekar@tekditechnologies.com>
2024-10-30 17:28:02 +0530

    "sonal issue resolved"

--- src/assets/mockdata/applicationList.tsx
+++ src/assets/mockdata/applicationList.tsx
@@ -29,34 +29,4 @@ export const applicationList = [
     internal_application_id: "APP102",
     status: "Submitted",
   },
-  {
-    benefit_id: "1",
-    application_name: "Scholarship Program A",
-    internal_application_id: "APP123",
-    status: "Approved",
-  },
-  {
-    benefit_id: "2",
-    application_name: "Housing Benefit Application",
-    internal_application_id: "APP456",
-    status: "Submitted",
-  },
-  {
-    benefit_id: "3",
-    application_name: "Education Assistance",
-    internal_application_id: "APP789",
-    status: "Rejected",
-  },
-  {
-    benefit_id: "4",
-    application_name: "Health Coverage Application",
-    internal_application_id: "APP101",
-    status: "Approved",
-  },
-  {
-    benefit_id: "5",
-    application_name: "Emergency Relief Fund",
-    internal_application_id: "APP102",
-    status: "Submitted",
-  },
 ];

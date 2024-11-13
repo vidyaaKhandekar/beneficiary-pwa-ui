8d2d113875d0ba04b041836cce06469269e28812
Manoj L <manojLondhe@users.noreply.github.com>
2024-11-13 17:40:04 +0530

    Update FilterData.tsx

--- src/assets/mockdata/FilterData.tsx
+++ src/assets/mockdata/FilterData.tsx
@@ -10,10 +10,10 @@ export const Gender: FilterOption[] = [
 ];
 export const IncomeRange: FilterOption[] = [
   { id: 1, label: "All", value: "" },
-  { id: 2, label: "Below 1,00,000", value: "1,00,000" },
-  { id: 3, label: "Below 2,50,000", value: "2,50,000" },
-  { id: 4, label: "Below 5,00,000", value: "5,00,000" },
-  { id: 5, label: "Below 7,50,000", value: "7,50,000" },
+  { id: 2, label: "Below 1,00,000", value: "100000" },
+  { id: 3, label: "Below 2,50,000", value: "250000" },
+  { id: 4, label: "Below 5,00,000", value: "500000" },
+  { id: 5, label: "Below 7,50,000", value: "750000" },
   { id: 6, label: "Other", value: "other" },
 ];
 export const Castes: FilterOption[] = [
@@ -24,6 +24,6 @@ export const Castes: FilterOption[] = [
   { id: 5, label: "General", value: "general" },
 ];
 export const BenefitAmount: FilterOption[] = [
-  { id: 1, label: "12,000", value: "12,000" },
-  { id: 2, label: "35,000", value: "35,000" },
+  { id: 1, label: "12,000", value: "12000" },
+  { id: 2, label: "35,000", value: "35000" },
 ];

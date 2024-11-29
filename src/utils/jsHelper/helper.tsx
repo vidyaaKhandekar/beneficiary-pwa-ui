53b4171f3d0226ae95960cda7ec71ed902c2b745
Vidya Hanumant Khandekar <vidya.khandekar@tekditechnologies.com>
2024-11-29 16:56:48 +0530

    Profile changes (#41)
    * Routing issue resolved
    
    * response key mapping changes
    
    * Style issues fixed
    
    * Sonar issues resolved
    
    * coderabbitai comments resolved
    
    * Sonar cloud issue resolved
    
    * Sonar cloud issue resolved
    
    * style issues fixed
    
    * style changed
    
    * Consent Dialogue added
    
    * conflict resolved
    
    * Sonar issue resolved
    
    * Sonar issue resolved
    
    * comments resolved
    
    * layout issue fixed
    
    * navigation issue fixed
    
    * key added
    
    * env changes
    
    * style change
    
    * mapping changes
    
    * Fix-Filter
    
    * Fix-FIlter Style change
    
    * Added transform data
    
    * Profile changes
    
    * added type in userdata

--- src/utils/jsHelper/helper.tsx
+++ src/utils/jsHelper/helper.tsx
@@ -266,3 +266,12 @@ export const transformData = (userData) => {
     mobile: userData.phoneNumber ?? "",
   };
 };
+
+export const formatDate = (dateString) => {
+  const date = new Date(dateString);
+  const day = String(date.getUTCDate()).padStart(2, "0");
+  const month = String(date.getUTCMonth() + 1).padStart(2, "0");
+  const year = date.getUTCFullYear();
+
+  return `${day}/${month}/${year}`;
+};
